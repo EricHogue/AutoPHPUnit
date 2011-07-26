444b02dd07d8c6ed34d60225b65ffb2c1850d06c
Eric Hogue <eric@erichogue.ca>
2011-07-26 05:50:09 -0400

    Initial commit. Adding to GitHub

---
+++ AutoPHPUnit.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Automaticaly run the PHPUnit tests everytime a file is saved
+ *
+ *
+ * Make it recursive
+ * Clear the screen
+ * Get the path
+ */
+class AutoPHPUnit
+{
+	/**
+	 * @var string
+	 */
+	private $pathToTest;
+
+	/**
+	 * @var string
+	 */
+	private $autoPHPUnitPath;
+
+
+
+	/**
+	 * Class constructor
+	 */
+	public function __construct()
+	{
+		$this->pathToTest = getcwd();
+		$this->autoPHPUnitPath = realpath(dirname(__FILE__));
+	}
+
+	/**
+	 * Run the program
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$this->clearConsole();
+		$this->watchFiles();
+	}
+
+	/**
+	 * Wait for a change in the file system
+	 *
+	 * @return void
+	 */
+	protected function watchFiles() {
+		require $this->autoPHPUnitPath . '/RecursiveFolderWatcher.php';
+
+		$o = new RecursiveFolderWatcher($this->pathToTest, array($this, 'runUnitTests'));
+		$o->watch();
+	}
+
+	/**
+	 * Callback function to run the unit tests
+	 *
+	 * @return void
+	 */
+	public function runUnitTests() {
+		$this->clearConsole();
+
+		$command = 'phpunit -c ' . $this->pathToTest . '/phpunit.xml ' . $this->pathToTest;
+		error_log($command);
+		echo `$command`;
+		error_log('Done');
+	}
+
+	/**
+	 * Clear the console
+	 *
+	 * @return void
+	 */
+	protected function clearConsole() {
+		passthru('clear');
+	}
+}
+
+$runner = new AutoPHPUnit();
+$runner->run();
---
+++ RecursiveFolderWatcher.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * Watch a folder for any changes on files
+ */
+class RecursiveFolderWatcher
+{
+	/**
+	 * @var string
+	 */
+	private $pathToWatch;
+
+	/**
+	 * @var resource
+	 */
+	private $inotify;
+
+	/**
+	 * @var array
+	 */
+	private $watchDescriptors = array();
+
+	/**
+	 * @var function
+	 */
+	private $callbackFunction;
+
+	/**
+	 * @var boolean
+	 */
+	private $testsHaveRunForThisWatch = false;
+
+
+	/**
+	 * Class constructor
+	 */
+	public function __construct($pathToWatch, $callbackFunction)
+	{
+		$this->pathToWatch = $pathToWatch;
+		$this->callbackFunction = $callbackFunction;
+	}
+
+
+	/**
+	 * Watch a folder for any changes in files
+	 *
+	 * @return void
+	 */
+	public function watch() {
+		$this->inotify = inotify_init();
+
+		$this->addWatch($this->pathToWatch);
+
+		while (true) {
+			$events = inotify_read($this->inotify);
+
+			if ($events) {
+				$this->testsHaveRunForThisWatch = false;
+				array_walk($events, array($this, 'handleEvent'));
+			}
+
+			usleep(1000);
+		}
+	}
+
+	/**
+	 * Add a watch on a folder
+	 *
+	 * @return void
+	 */
+	protected function addWatch($folderPath) {
+		$watchDescriptor = inotify_add_watch($this->inotify, $folderPath, IN_ALL_EVENTS);
+		$this->watchDescriptors[$watchDescriptor] = $folderPath;
+
+		$this->addWatchToSubFolders($folderPath);
+	}
+
+	/**
+	 * Add watches to sub folders
+	 *
+	 * @return void
+	 */
+	protected function addWatchToSubFolders($parentFolder) {
+		$iterator = new DirectoryIterator($parentFolder);
+
+		foreach ($iterator as $fileInfo) {
+			if ($fileInfo->isDir() && !$fileInfo->isDot()) {
+				$this->addWatch($fileInfo->getPathname());
+			}
+		}
+	}
+
+	/*
+	 * Check if there is anything to do with an event
+	 *
+	 * @return void
+	 */
+	protected function handleEvent(array $event) {
+		if ($this->doesActionRequiresRunningUnitTest($event['mask'])) {
+			$this->runUnitTests();
+		}
+
+		if ($this->actionIsForNewFolder($event['mask'])) {
+			$folderPath = $this->getPathForWatchDescriptor($event['wd']) . $event['name'];
+			$this->addWatch($folderPath);
+		}
+	}
+
+	/*
+	 * Check it the action is for the creation of a new folder
+	 *
+	 * @return void
+	 */
+	protected function actionIsForNewFolder($mask) {
+		return (IN_CREATE === (IN_CREATE & $mask) && IN_ISDIR === (IN_ISDIR & $mask));
+	}
+
+	/*
+	 * Cehck if the action required running the unit tests
+	 *
+	 * @return void
+	 */
+	protected function doesActionRequiresRunningUnitTest($mask) {
+		$needsUnitTestRun = false;
+
+		if (IN_MODIFY === (IN_MODIFY & $mask)) {
+			$needsUnitTestRun = true;
+		}
+
+		if ((IN_CREATE === (IN_CREATE & $mask) && 0 === (IN_ISDIR & $mask))) {
+			$needsUnitTestRun = true;
+		}
+
+
+		return $needsUnitTestRun;
+	}
+
+
+	/*
+	 * Return the path for a descriptor
+	 *
+	 * @return string
+	 */
+	protected function getPathForWatchDescriptor($watchDescriptor) {
+		if (array_key_exists($watchDescriptor, $this->watchDescriptors)) {
+			return $this->watchDescriptors[$watchDescriptor] . '/';
+		} else {
+			return '';
+		}
+	}
+
+
+	/*
+	 * Run the unit tests
+	 *
+	 * @return void
+	 */
+	protected function runUnitTests() {
+		if (!$this->testsHaveRunForThisWatch) {
+			$this->testsHaveRunForThisWatch = true;
+			call_user_func($this->callbackFunction);
+		}
+	}
+
+
+
+
+}
